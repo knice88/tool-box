ae550b0a92da2993941939d35a35e2f92e568a80
lin <none@example.com>
2024-10-29 22:40:52 +0800

    增加快捷键

--- src/views/Sidebar.vue
+++ src/views/Sidebar.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { Search } from '@element-plus/icons-vue'
-import { ref, computed } from 'vue';
+import { ref, computed, useTemplateRef, onMounted } from 'vue';
+import { useRouter } from 'vue-router'
+const router = useRouter()
 const list = [
     {
         "route": "/json",
@@ -36,11 +38,22 @@ const displayMenus = computed(() => {
     console.log(searchWords.value)
     return list.filter(item => item.title.toLowerCase().includes(searchWords.value.toLowerCase()))
 })
+const searchEle = useTemplateRef('search')
+onMounted(() => {
+    // 打开页面时，搜索框自动获得焦点
+    searchEle.value.focus()
+})
+const openFirstMenu = () => {
+    if (displayMenus.value.length > 0) {
+        const firstMenu = displayMenus.value[0].route
+        router.push(firstMenu)
+    }
+}
 </script>
 <template>
     <el-menu class="el-menu-vertical" router>
         <el-input placeholder="搜索菜单" style="width: 90%;margin-right: 5px;" :suffix-icon="Search" clearable
-            v-model="searchWords"></el-input>
+            v-model="searchWords" ref="search" @keyup.enter="openFirstMenu" />
         <el-menu-item v-for="item in displayMenus" :key="item.route" :index="item.route" :route="item.route"> {{
             item.title }}
         </el-menu-item>
