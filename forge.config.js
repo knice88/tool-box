1ae05ad413ef39957dfb7710dbb62da1ed8ce505
knice88 <knice88ljb@gmail.com>
2024-10-29 13:02:02 +0800

    样式修改

--- forge.config.js
+++ forge.config.js
@@ -22,32 +22,32 @@ module.exports = {
     // 由于@electron-forge/plugin-vite插件存在问题，导致运行npm run make时，生成的安装包无法运行，因此需要执行两次npm runmake
     // 第一次运行npm run make时，打开plugin-vite，生成.vite文件夹，这时候out目录中的安装包还是无法运行的，可以直接删掉
     // 第二次运行，注释掉此配置，再次运行得到可用的安装包
-    // {
-    //   name: '@electron-forge/plugin-vite',
-    //   config: {
-    //     // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-    //     // If you are familiar with Vite configuration, it will look really familiar.
-    //     build: [
-    //       {
-    //         // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-    //         entry: 'src/main.js',
-    //         config: 'vite.main.config.mjs',
-    //         target: 'main',
-    //       },
-    //       {
-    //         entry: 'src/preload.js',
-    //         config: 'vite.preload.config.mjs',
-    //         target: 'preload',
-    //       },
-    //     ],
-    //     renderer: [
-    //       {
-    //         name: 'main_window',
-    //         config: 'vite.renderer.config.mjs',
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      name: '@electron-forge/plugin-vite',
+      config: {
+        // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+        // If you are familiar with Vite configuration, it will look really familiar.
+        build: [
+          {
+            // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+            entry: 'src/main.js',
+            config: 'vite.main.config.mjs',
+            target: 'main',
+          },
+          {
+            entry: 'src/preload.js',
+            config: 'vite.preload.config.mjs',
+            target: 'preload',
+          },
+        ],
+        renderer: [
+          {
+            name: 'main_window',
+            config: 'vite.renderer.config.mjs',
+          },
+        ],
+      },
+    },
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
     new FusesPlugin({
