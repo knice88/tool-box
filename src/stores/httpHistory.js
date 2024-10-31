c986d58a1b0a895cc9b0067d3cd07da2fc6b5e4c
knice88 <knice88ljb@gmail.com>
2024-10-31 22:30:53 +0800

    http客户端历史记录

---
+++ src/stores/httpHistory.js
@@ -0,0 +1,21 @@
+import { da } from 'element-plus/es/locales.mjs'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useHttpHistory = defineStore('httpHistory', () => {
+    // 接口调用的历史记录
+    const dataList = ref([])
+    // 添加一个新项
+    const addItem = (item) => {
+        // 添加到列表
+        dataList.value.push(item)
+        // 最多只保存20个
+        if (dataList.value.length > 20) {
+            dataList.value.shift()
+        }
+    }
+    const delItem = (index) => {
+        dataList.value.splice(index, 1)
+    }
+    return { dataList, addItem, delItem }
+}, { persist: true })
