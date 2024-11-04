73ab3071dfd41a11a9bfc0f312c1168bf364fbd2
knice88 <knice88ljb@gmail.com>
2024-11-04 20:28:22 +0800

    引入json-editor插件

--- src/views/Json.vue
+++ src/views/Json.vue
@@ -1,30 +1,25 @@
 <script setup>
 import { ref } from 'vue'
-import { ElMessage } from 'element-plus'
-import { copyToClipboard } from '@/composables/clipboard'
+import JsonEditorVue from 'json-editor-vue3'
 
 const jsonStr = ref('')
-const clickFormat = (space) => {
-    // 1、JSON.parse：把JSON字符串转换为JSON对象
-    // 2、JSON.stringify：把JSON对象 转换为 有缩进的 JSON字符串格式
-    let jsonObj;
-    try {
-        jsonObj = JSON.parse(jsonStr.value)
-    } catch (error) {
-        ElMessage.error('格式错误: ' + error.message)
-        return
-    }
-    jsonStr.value = JSON.stringify(jsonObj, null, space)
-}
-
 </script>
 
 <template>
-    <div>
-        <el-button type="primary" @click="clickFormat('\t')">格式化JSON</el-button>
-        <el-button type="primary" @click="clickFormat('')">压缩</el-button>
-        <el-button type="primary" @click="copyToClipboard(jsonStr)">复制</el-button>
-        <br /><br />
-        <el-input v-model="jsonStr" type="textarea" :autosize="{ minRows: 20, maxRows: 40 }" placeholder="请输入JSON字符串" />
+    <div class="json-viewer-container">
+        <JsonEditorVue v-model="jsonStr" currentMode="code" class="content" />
     </div>
 </template>
+
+<style scoped>
+.json-viewer-container {
+    width: 100%;
+    height: 800px;
+}
+
+.json-viewer-container>.content {
+    overflow: auto;
+    width: 100%;
+    height: 100%;
+}
+</style>
--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteCommonjs()],
   resolve: {
     // 实际的路径转换  @  -> src
     alias: {
