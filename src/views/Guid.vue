8220935ab6136b8649172815bd75729a2b0a5cb1
knice88 <knice88ljb@gmail.com>
2024-11-17 20:57:18 +0800

    生成guid添加前缀后缀

--- src/views/Guid.vue
+++ src/views/Guid.vue
@@ -2,10 +2,10 @@
 import { ref, watch } from 'vue'
 import { v4 as uuidv4 } from 'uuid';
 
-const config = ref({ count: 1 })
+const config = ref({ count: 1, prefix: '', suffix: '' })
 const resultText = ref('')
 watch(config, () => {
-    const { count, isUpper, hasHyphen } = config.value
+    const { count, isUpper, hasHyphen, prefix, suffix } = config.value
     const result = []
     for (let i = 0; i < count; i++) {
         let str = uuidv4()
@@ -15,7 +15,7 @@ watch(config, () => {
         if (!hasHyphen) {
             str = str.replace(/-/g, '')
         }
-        result.push(str)
+        result.push(prefix + str + suffix)
     }
     resultText.value = result.join('\n')
 }, { deep: true, immediate: true })
@@ -31,6 +31,18 @@ watch(config, () => {
     <el-input style="width: 280px" v-model.number="config.count" min="1">
         <template #prepend>
             数量
+        </template>
+    </el-input>
+    &nbsp;
+    <el-input style="width: 280px" v-model="config.prefix">
+        <template #prepend>
+            UUID前缀
+        </template>
+    </el-input>
+    &nbsp;
+    <el-input style="width: 280px" v-model="config.suffix">
+        <template #prepend>
+            UUID后缀
         </template>
     </el-input>
     <br>
