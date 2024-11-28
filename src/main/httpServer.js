23b6508295f89ef341a9815a560e065ba4e28aad
knice88 <knice88ljb@gmail.com>
2024-11-28 13:47:26 +0800

    上传文件页面样式修改；保存文件的修改时间

--- src/main/httpServer.js
+++ src/main/httpServer.js
@@ -55,29 +55,67 @@ export default {
                             <title>文件上传</title>
                         </head>
                         <body>
-                            <form action="/rec" method="post" enctype="multipart/form-data">
                                 <input id="fileInput" type="file" name="file" multiple style="display: none">
+                                <input type="hidden" id="fileCreatedTime" name="_createdTimes">
                                 <div id="fileInputBtn" style="border: 2px dashed #ccc; padding: 100px; text-align: center; cursor: pointer;">点击选择文件</div>
-                                <textarea name="textMsg" style="display: block; margin: 10px 0; width: 100%; height: 200px; border: 2px dashed #ccc;" placeholder="输入文本"></textarea>
-                                <br>
-                                <input type="submit" value="发送">
-                            </form>
+                                <textarea id="textMsg" name="textMsg" style="display: block; margin: 10px 0; width: 100%; height: 200px; border: 2px dashed #ccc;" placeholder="输入文本"></textarea>
+                                <div id="sendBtn" style="width: 20%; height: 40px; background-color: #409eff; text-align: center; line-height: 40px; font-size: 20px; color: white; padding: 10px 50px;border-radius: 10px;">发送</div>
                         </body>
                         <script>
                         const fileInput = document.getElementById('fileInput');
                         const fileInputBtn = document.getElementById('fileInputBtn');
+                        const sendBtn = document.getElementById('sendBtn');
                         fileInputBtn.addEventListener('click', (event) => {
                             fileInput.click();
                         });
                         fileInput.addEventListener('change', (event) => {
-                            const files = Array.from(fileInput.files); // 获取选中的文件
-                            if (files.length > 0) {
-                                const fileNames = files.map(file => file.name).join(', '); // 获取文件名
-                                fileInputBtn.textContent = fileNames; // 更新按钮文本
+                            let fileTime = {}
+                            let totalSize = 0;
+                            let fileNames = [];
+                            for (let i = 0; i < event.target.files.length; i++) {
+                                const file = event.target.files[i];
+                                fileTime[file.name] = file.lastModified; // 获取最后修改时间（用于创建时间）
+                                totalSize += file.size;
+                                fileNames.push(file.name);
+                            }
+                            document.getElementById('fileCreatedTime').value = JSON.stringify(fileTime); // 将文件名和创建时间序列化为字符串
+                            if (totalSize > 0) {
+                                fileInputBtn.innerHTML = '<p>'+fileNames.join('</p><p>')+'</p><p>总大小：'+showSize(totalSize)+'</p>'; // 更新按钮文本
                             } else {
                                 fileInputBtn.textContent = '点击选择文件'; // 如果没有选择文件，重置文本
                             }
                         });
+                        sendBtn.addEventListener('click', (event) => {
+                        const fileInput = document.getElementById('fileInput');
+                        const textMsg = document.getElementById('textMsg');
+                            const formData = new FormData();
+                            formData.append('textMsg', textMsg.value);
+                            for (let i = 0; i < fileInput.files.length; i++) {
+                                const file = fileInput.files[i];
+                                formData.append('file', file, file.lastModified+'_'+file.name);// 文件名改为 时间戳_文件名
+                            }
+                            fetch('/rec', {
+                                method: 'POST',
+                                body: formData
+                            })
+                            .then(response => response.json())
+                            .then(data => {
+                                alert(data.message);
+                            })
+                            .catch(error => {
+                                alert(error);
+                            });
+                        });
+                        function showSize(sizeByte) {
+                            if (sizeByte < 1024) {
+                                return sizeByte + 'B';
+                            } else if (sizeByte < 1024 * 1024) {
+                                return (sizeByte / 1024).toFixed(2) + 'KB';
+                            } else if (sizeByte < 1024 * 1024 * 1024) {
+                                return (sizeByte / 1024 / 1024).toFixed(2) + 'MB';
+                            }
+                            return (sizeByte / 1024 / 1024 / 1024).toFixed(2) + 'GB';
+                        }
                         </script>
                     </html>
                 `
@@ -88,31 +126,44 @@ export default {
                 // 处理Content-Type为multipart/form-data的请求
                 const bb = busboy({ headers: req.headers, defParamCharset: 'utf-8', defCharset: 'utf-8' });
                 let successTip = ''
+                const downloadDir = getDownloadDir()
                 bb.on('file', (name, file, info) => {
                     // 只传文本的时候，还是会走到这里，但是info.filename为undefined
                     // 所以需要判断一下info.filename是否存在
                     if (info.filename) {
-                        let saveTo = path.join(getDownloadDir(), info.filename)
-                        file.pipe(fs.createWriteStream(saveTo))
-                        successTip += `上传成功，文件保存到：${saveTo}<br>`
+                        // 上传的时候，文件名改为 时间戳_文件名，所以这里需要将文件名改回来
+                        const lastModified = info.filename.substring(0, info.filename.indexOf('_'))
+                        const fileName = info.filename.substring(info.filename.indexOf('_') + 1)
+                        let fullPath = path.join(downloadDir, fileName)
+                        // 创建可写流并保存文件
+                        const writeStream = fs.createWriteStream(fullPath);
+                        file.pipe(writeStream)
+                        writeStream.on('finish', () => {
+                            // 当文件写入完成后，设置文件的访问时间和修改时间
+                            const lastModifiedTime = Number(lastModified) || Date.now(); // 使用提供的最后修改时间，默认为当前时间
+                            const parsedTime = new Date(lastModifiedTime);
+                            // 设置文件的访问和修改时间
+                            fs.utimes(fullPath, parsedTime, parsedTime, (err) => {
+                                if (err) {
+                                    console.error(`设置文件时间失败: ${err} ${parsedTime}`);
+                                } else {
+                                    console.log(`设置文件 ${fullPath} 的时间成功`);
+                                }
+                            });
+                        });
+                        successTip += `上传成功，文件保存到：${downloadDir}\n`
                     }
                 });
                 bb.on('field', (name, val, info) => {
                     // 处理文本字段
                     if (val) {
                         BrowserWindow.getAllWindows()[0].webContents.send('form-msg-updated', val)
-                        successTip += '消息发送成功<br>'
-                        bb.emit('close');
+                        successTip += '消息发送成功\n'
                     }
                 });
                 bb.on('close', () => {
                     res.writeHead(200, { 'Connection': 'close' });
-                    res.end(`<html>
-                        <head>
-                            <meta charset="UTF-8" />
-                        </head>
-                        <body>${successTip}</body>
-                        </html>`);
+                    res.end(JSON.stringify({ success: true, message: successTip }));
                 });
                 req.pipe(bb);
             }
